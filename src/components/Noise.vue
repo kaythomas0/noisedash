e8db936cdf3784c38d6d24fbf3fb2bcc2e13c86f
Kevin Thomas <me@kevinthomas.dev>
2021-07-18 00:30:40 -0700

    Add duration setting

--- src/components/Noise.vue
+++ src/components/Noise.vue
@@ -2,6 +2,21 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Noisedash
+        </h1>
+      </v-col>
+
+      <v-col cols="12">
         <v-btn
           :disabled="isDisabled"
           @click="handleStart"
@@ -9,12 +24,20 @@
           Start
         </v-btn>
       </v-col>
+
       <v-col cols="12">
         <v-btn
           @click="handleStop"
         >
           Stop
         </v-btn>
+      </v-col>
+
+      <v-col cols="12">
+        <v-text-field
+          v-model="seconds"
+          label="Seconds"
+        />
       </v-col>
     </v-row>
   </v-container>
@@ -27,18 +50,19 @@
     name: 'Noise',
 
     data: () => ({
-      isDisabled: false
+      isDisabled: false,
+      seconds: 4
     }),
     created() {
-      this.noise = new Noise({volume: -10, type: "brown"}).toDestination()
     },
     methods: {
       handleStart() {
         this.isDisabled = true
-        Transport.scheduleOnce((time) => {
-          this.noise.start(time).stop(time + 2)
-        })
+        Transport.cancel()
+        this.noise = new Noise({volume: -10, type: "brown"}).toDestination()
+        this.noise.sync().start(0).stop(this.seconds)
         Transport.start()
+        Transport.loopEnd = this.seconds
       },
       handleStop() {
         Transport.stop()
