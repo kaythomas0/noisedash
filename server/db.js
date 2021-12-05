a12a4cae7c077babbe1fe12be57fd631952004b5
Kevin Thomas <me@kevinthomas.dev>
2021-12-05 00:31:58 -0800

    Fix db creation path

--- server/db.js
+++ server/db.js
@@ -1,7 +1,8 @@
 const sqlite3 = require('sqlite3')
 const fs = require('fs')
+const path = require('path')
 
-if (!fs.existsSync('db')) {
-  fs.mkdirSync('db')
+if (!fs.existsSync(path.join(__dirname, '../db'))) {
+  fs.mkdirSync(path.join(__dirname, '../db'))
 }
 module.exports = new sqlite3.Database('db/db.sqlite3')
